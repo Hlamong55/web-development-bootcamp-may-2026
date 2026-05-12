3e5c94ef395f2ad004bb88b1598ce981796c1da0
Hlamong55 <bijoymarma55@gmail.com>
2026-05-13 04:27:52 +0600

    connect mongodb and configure backend database

--- Hlamong55/server/server.js
+++ Hlamong55/server/server.js
@@ -3,8 +3,11 @@ require("dotenv").config();
 const http = require("http");
 
 const app = require("./src/app");
+const connectDB = require("./src/config/db");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
+
+connectDB();
 
 const server = http.createServer(app);
 
